e303f54a1dd1f13c645660c3744a34977a533e17
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-12-04 17:33:23 +0100

    add background color to page

--- src/components/side/Bio.tsx
+++ src/components/side/Bio.tsx
@@ -9,11 +9,11 @@ export const Bio: Component = () => {
     <div>
       <h5>About the author</h5>
       <div class="card">
-        <img
+        {/* <img
           src={authorsPortrait}
           class="card-img-top"
           alt="Author's Portrait"
-        />
+        /> */}
         <div class="card-body">
           <p class="card-text">
             <strong>Floppy</strong> is an Advocate for UX and founder of this
